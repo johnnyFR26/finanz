2c087893950003a3e9459b5a172e35521d8c6bb8
johnnyFR26 <149261007+johnnyFR26@users.noreply.github.com>
2025-03-18 11:25:15 -0300

    [add: transaction modal]

--- src/app/pages/home/account/account.component.ts
+++ src/app/pages/home/account/account.component.ts
@@ -1,21 +1,41 @@
 import { CurrencyPipe } from "@angular/common";
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 import { AccountService } from "../../../services/account.service";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialog } from "@angular/material/dialog";
+import { TransactionModalComponent } from "./transactions/transaction-modal.component";
+import { UserService } from "../../../services/user.service";
 
 @Component({
     selector: 'app-account',
     template: `
         <h1>Conta: {{account()?.currentValue | currency: 'BRL'}}</h1>
+        <br>
+        <hr>
+        <button mat-fab extended color="primary" (click)="openDialog()">
+        <mat-icon>payment</mat-icon>Depositar</button>
     `,
     styleUrl: './account.component.scss',
-    imports: [CurrencyPipe]
+    imports: [CurrencyPipe, MatIconModule, MatButtonModule]
 })
 export class AccountComponent implements OnInit{
-
+    
     ngOnInit(): void {     
         console.table(this.account)
     }
     private accountService = inject(AccountService)
+    private userService = inject(UserService)
+    protected user = this.userService.getUserInfo()
     protected account = this.accountService.getCurrentAccount()
+    readonly dialog = inject(MatDialog);
+    public name = signal('Lucas');
+    public animal = signal('Dog');
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TransactionModalComponent, {
+      data: {name: this.user()?.user.name, animal: this.animal()},
+    });
+}
 
 }
---
+++ src/app/pages/home/account/transactions/transaction-modal.component.ts
@@ -0,0 +1,54 @@
+import {ChangeDetectionStrategy, Component, inject, model, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+@Component({
+    selector: 'app-transaction-modal',
+    template: `
+    <h2 mat-dialog-title>Enviar de {{data.name}}</h2>
+    <mat-dialog-content>
+      <p>Nova tranferencia</p>
+      <mat-form-field>
+        <mat-label>Transferir</mat-label>
+        <input matInput [(ngModel)]="value" />
+      </mat-form-field>
+    </mat-dialog-content>
+    <mat-dialog-actions>
+      <button mat-button (click)="onNoClick()">No Thanks</button>
+      <button mat-button [mat-dialog-close]="animal()" cdkFocusInitial>Ok</button>
+    </mat-dialog-actions>
+    `,
+    styleUrls: ['./transaction-modal.component.scss'],
+    imports: [    
+        MatFormFieldModule,
+        MatInputModule,
+        FormsModule,
+        MatButtonModule,
+        MatDialogTitle,
+        MatDialogContent,
+        MatDialogActions,
+        MatDialogClose,
+      ]
+})
+export class TransactionModalComponent{
+    readonly dialogRef = inject(MatDialogRef<TransactionModalComponent>);
+    readonly data = inject<any>(MAT_DIALOG_DATA);
+    readonly animal = model(this.data.animal);
+    value = 0;
+    protected valueSignal = signal('0');
+  
+    onNoClick(): void {
+      this.dialogRef.close();
+    }
+}
