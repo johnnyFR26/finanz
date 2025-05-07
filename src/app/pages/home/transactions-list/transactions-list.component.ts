6143d26233c58d8864f942b6fe14e7fcc5f72d28
alygab <alysongabriel07@gmail.com>
2025-05-07 10:38:20 -0300

    [add current value in statement]

--- src/app/pages/home/transactions-list/transactions-list.component.ts
+++ src/app/pages/home/transactions-list/transactions-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from "@angular/material/icon";
 import { MatTableModule } from '@angular/material/table';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 
@@ -9,39 +10,16 @@ import { AccountService } from '../../../services/account.service';
 @Component({
   selector: 'app-transactions-list',
   standalone: true,
-  imports: [CommonModule, MatTableModule, CurrencyPipe, DatePipe],
+  imports: [CommonModule, MatTableModule, CurrencyPipe, DatePipe, MatIconModule],
   template: `
-    <h1>Transações</h1>
-
-    <table mat-table [dataSource]="transactions()" class="mat-elevation-z8" style="width: 100%;">
-
-      <!-- ID Column -->
-      <ng-container matColumnDef="destination">
-        <th mat-header-cell *matHeaderCellDef> Destino </th>
-        <td mat-cell *matCellDef="let transaction"> {{transaction.destination}} </td>
-      </ng-container>
-
-      <!-- Value Column -->
-      <ng-container matColumnDef="value">
-        <th mat-header-cell *matHeaderCellDef> Valor </th>
-        <td mat-cell *matCellDef="let transaction"> {{transaction.value | currency:'BRL'}} </td>
-      </ng-container>
-
-      <!-- Type Column -->
-      <ng-container matColumnDef="type">
-        <th mat-header-cell *matHeaderCellDef> Tipo </th>
-        <td mat-cell *matCellDef="let transaction"> {{transaction.type}} </td>
-      </ng-container>
-
-      <!-- CreatedAt Column -->
-      <ng-container matColumnDef="createdAt">
-        <th mat-header-cell *matHeaderCellDef> Criado em </th>
-        <td mat-cell *matCellDef="let transaction"> {{transaction.createdAt | date:'short'}} </td>
-      </ng-container>
-
-      <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-      <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
-    </table>
+    <div class="box">
+      <div class="currency">
+        <h2>SALDO ATUAL</h2>
+        <h1>{{account()?.currentValue | currency: 'BRL'}}</h1>
+        <mat-icon>account_balance</mat-icon>
+      </div>
+      <div class=""></div>
+    </div>
   `,
   styleUrls: ['./transaction-list.component.scss'],
 })
