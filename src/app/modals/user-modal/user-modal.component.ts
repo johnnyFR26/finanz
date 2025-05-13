0ef336f22a8bc3ea664db77db4d063394b214936
johnny Fontes Rabelo <johnnyfr26@MacBook-Pro-de-johnny.local>
2025-05-13 00:35:48 -0300

    add button to delete user

---
+++ src/app/modals/user-modal/user-modal.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatDialogActions, MatDialogRef } from "@angular/material/dialog";
+import { AccountService } from "../../services/account.service";
+import { UserService } from "../../services/user.service";
+
+@Component({
+    selector: 'user-modal',
+    template: `
+    <div class="content">
+      <p>Configurações</p>
+
+
+      <p>Deseja excluir a conta?</p>
+
+    <mat-dialog-actions>
+      <button mat-button cancelar (click)="onNoClick()">Cancelar</button>
+      <button mat-button excluir (click)="onSubmit()" cdkFocusInitial>Ok</button>
+    </mat-dialog-actions>
+    </div>
+   
+    `,
+    styleUrls: ['./user-modal.component.css'],
+    imports: [MatDialogActions, FormsModule],
+})
+export class UserModalComponent {
+    private accountService = inject(AccountService)
+    private userService = inject(UserService)
+    readonly dialogRef = inject(MatDialogRef<UserModalComponent>)
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+    onSubmit(): any {
+        this.userService.deleteUser()
+        this.dialogRef.close()
+    }
+
+}
