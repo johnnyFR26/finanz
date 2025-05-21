6d573bab84fae53bc39c513c5a06da400b4f34f0
alygab <alysongabriel07@gmail.com>
2025-05-21 10:46:06 -0300

    [add: transaction component]

---
+++ src/app/components/transaction/transaction.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, input, ChangeDetectionStrategy, signal, OnInit } from '@angular/core';
+import { MatIconModule } from "@angular/material/icon";
+import {MatExpansionModule} from '@angular/material/expansion';
+
+@Component({
+    selector: 'transaction',
+    imports: [MatIconModule, MatExpansionModule],
+    styleUrl: './transaction.component.scss',
+    template: `
+    <hr>
+    <mat-expansion-panel (opened)="panelOpenState.set(true)" (closed)="panelOpenState.set(false)">
+    <mat-expansion-panel-header>
+      <mat-panel-title> <button mat-fab><mat-icon>check_circle</mat-icon></button> </mat-panel-title>
+      <mat-panel-description>
+        Currently I am {{panelOpenState() ? 'open' : 'closed'}}
+      </mat-panel-description>
+    </mat-expansion-panel-header>
+    <p>{{transaction()?.description}}</p>
+  </mat-expansion-panel>
+    `
+})
+
+export class TransactionComponent implements OnInit{
+  readonly panelOpenState = signal(false);
+  readonly transaction = input<any>();
+  
+  ngOnInit(): void {
+      console.log(this.transaction())
+  }
+}
