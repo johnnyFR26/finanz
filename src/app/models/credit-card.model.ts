b4aafeca2efceb7e1a29c7ca3ba88ab5fd646410
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-08-26 13:12:29 -0300

    feat: adiciona service para creditcards

---
+++ src/app/models/credit-card.model.ts
@@ -0,0 +1,8 @@
+export interface CreditCardModel {
+    id: string
+    name: string
+    number: string
+    cvv: string
+    expirationDate: string
+    type: string
+}
--- src/app/services/credit-card.service.ts
+++ src/app/services/credit-card.service.ts
@@ -1,7 +1,40 @@
-import { Injectable } from "@angular/core";
+import { effect, inject, Injectable, signal } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { CreditCardModel } from "../models/credit-card.model";
+import { AccountService } from "./account.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CreditCardService {
+    private urlApi = environment.urlApi
+    private http = inject(HttpClient)
+    private creditCards = signal<CreditCardModel[]>([])
+    private accountService = inject(AccountService)
+    private account = this.accountService.getCurrentAccount()
+    constructor(){
+        effect(() => {
+            this.getCreditCards()
+        })
+    }
+    setCreditCards(creditCards: CreditCardModel[]){
+        this.creditCards.set(creditCards)
+    }
+    getCreditCards(){
+        this.http.get<CreditCardModel[]>(`${this.urlApi}/creditCards/${this.account()?.id}`).subscribe({
+            next: (response) => {
+                this.setCreditCards(response)
+            },
+            error: (error) => {
+                console.log(error)
+            }
+        })
+    }
+
+    getCurrentCreditCard(){
+        return this.creditCards()
+    }
+
+
 }
