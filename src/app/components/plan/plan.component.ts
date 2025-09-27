7ce111821cd974b32a303f33beb452ee1c4894e3
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-09-27 19:55:40 -0300

    fix: corrige variaveis para computed signal

--- src/app/components/plan/plan.component.ts
+++ src/app/components/plan/plan.component.ts
@@ -1,6 +1,6 @@
 import { AccountService } from './../../services/account.service';
 import { MatIcon } from '@angular/material/icon';
-import { Component, inject, input } from "@angular/core";
+import { Component, computed, inject, input } from "@angular/core";
 import { CurrencyPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -25,9 +25,9 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
                 <div><span>META</span>{{plan().limit| currency: account()?.currency}}</div>
                 <div><span>PAGO</span>{{paid| currency: account()?.currency}}</div>
                 <div><span>PREVISTO</span>{{predicted| currency: account()?.currency}}</div>
-                <div><span>TOTAL</span>{{missing| currency: account()?.currency}}</div>
+                <div><span>TOTAL</span>{{missing()| currency: account()?.currency}}</div>
             </div>
-            <mat-progress-bar mode="determinate" [value]="percentage" [attr.data]="percentage"></mat-progress-bar>
+            <mat-progress-bar mode="determinate" [value]="percentage()" [attr.data]="percentage()"></mat-progress-bar>
             <span>RESTAM {{plan().available| currency: account()?.currency}}</span>
         </div>
     `
@@ -40,6 +40,6 @@ export class PlanComponent {
     
     protected paid = 150;
     protected predicted = 200;
-    protected missing = this.plan().limit - this.plan().available;
-    protected percentage = this.missing / this.plan().limit;
+    protected missing = computed(() => this.plan().limit - this.plan().availableLimit);
+    protected percentage = computed(() => this.missing() / this.plan().limit);
 }
