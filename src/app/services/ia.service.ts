71b5816c7e7e74d4c27459067747f2c5b8ef0d87
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-10-13 19:39:16 -0300

    refact(ia): adiciona userId na url do bot

--- src/app/services/ia.service.ts
+++ src/app/services/ia.service.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { environment } from "../../environments/environment";
+import { UserService } from "./user.service";
 
 @Injectable({
     providedIn: "root",
 })
 export class IaService {
     private http = inject(HttpClient)
+    private userService = inject(UserService)
     private urlApi = environment.urlApi
+    private user = this.userService.getUserInfo()
     
 
     sendMessage(message: string) {
-       return this.http.post(`${this.urlApi}/ai-agent/chat`, {
+       return this.http.post(`${this.urlApi}/ai-agent/chat${this.user()?.user.id}`, {
             "message": message
         })
     }
