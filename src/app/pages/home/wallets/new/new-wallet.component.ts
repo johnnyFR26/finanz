97ef23ecc3b490ca928c8d193e677a275392f320
alygab <alysongabriel07@gmail.com>
2025-10-28 08:34:37 -0300

    feat: new wallet screen unfinished

--- src/app/pages/home/wallets/new/new-wallet.component.ts
+++ src/app/pages/home/wallets/new/new-wallet.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, inject, signal } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { provideNativeDateAdapter } from "@angular/material/core";
+import { provideNativeDateAdapter, MatOption } from "@angular/material/core";
 import { MatFormField, MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import {MatDatepickerModule} from '@angular/material/datepicker';
@@ -9,46 +9,62 @@ import { AccountService } from "../../../../services/account.service";
 import { Router } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
+import { MatSelect } from "@angular/material/select";
 
 @Component({
     selector: "app-new-wallet",
     styleUrls: ["./new-wallet.component.scss"],
-    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatButtonModule, MatIcon],
+    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatButtonModule, MatIcon, MatOption, MatSelect],
     providers: [provideNativeDateAdapter()],
     template: `
-    <div class="new-credit-card">
-        <button class="back" mat-icon-button (click)="this.router.navigate(['/home/creditCard'])"><mat-icon>keyboard_backspace</mat-icon></button>
-        <h2 class="title">NOVO CARTÃO DE CRÉDITO</h2>
-        <form class="box" name="form" (ngSubmit)="onSubmit()">
-            <div>
-                <label for="name">Nome do Cartão</label>
-                <input class="input" type="text" id="name" name="name" [(ngModel)]="name" placeholder="Nome do Cartão" required>
+    <button class="back" mat-icon-button (click)="this.router.navigate(['/home/wallets'])"><mat-icon>keyboard_backspace</mat-icon></button>
+    <div class="box">
+        <h2>Criar Carteira de Investimentos nova</h2>
+        <div class="titleInput">
+            <label for="name">Título:</label>
+            <input class="input" type="text" id="name" name="name" [(ngModel)]="name" required>
+        </div>
+        
+        <div>
+            <label>ícone:</label>
+            <div class="select-icon">
+                <mat-select name="icon" [(ngModel)]="icon">
+                    @for (icon of icons; track $index) {
+                    <mat-option value="{{icon}}"><mat-icon>{{icon}}</mat-icon></mat-option>
+                    }
+                </mat-select>
+                <mat-icon class="icon">{{icon()}}</mat-icon>
             </div>
-            <div>
-                <label for="company">Empresa</label>
-                <input class="input" type="text" id="company" name="company" [(ngModel)]="company" placeholder="Empresa" required>
-            </div>
+        </div>
 
-            <div>
-                <label for="availableLimit">Limite Disponível</label>
-                <input class="input" type="number" id="availableLimit" [(ngModel)]="availableLimit" name="availableLimit" placeholder="Limite Disponível" required>
-            </div>
-            <div>
-                <label for="limit">Limite Total</label>
-                <input class="input" type="number" id="limit" name="limit" [(ngModel)]="limit" placeholder="Limite Total" required>
-            </div>
-            <div>
-                <label for="closingDate">Data de Encerramento</label>
-                <input class="input" type="number" id="closingDate" name="closingDate" [(ngModel)]="closingDate" placeholder="Data de Encerramento" required>
-            </div>
-            <div>
-                <label for="paymentDate">Data de Pagamento</label>
-                <input class="input" type="number" id="paymentDate" name="paymentDate" [(ngModel)]="paymentDate" placeholder="Data de Pagamento" required>
-            </div>
-            
+        <div class="radios">
+            <input type="radio" name="type" id="daily">
+            <label for="daily">
+                Rendimento diário
+                <mat-icon>calendar_today</mat-icon>
 
-            <button class="button" type="submit">Salvar</button>
-        </form>
+            </label>
+            <input type="radio" name="type" id="monthly">
+            <label for="monthly">
+                Rendimento mensal
+                <mat-icon>calendar_today</mat-icon>
+            </label>
+        </div>
+        <div>
+            <label for="limit">Porcentagem(%) do rendimento:</label>
+            <input class="input percent" type="number" id="limit" name="limit" [(ngModel)]="limit">
+        </div>
+        <div>
+            <label for="closingDate">Data de Encerramento</label>
+            <input class="input" type="number" id="closingDate" name="closingDate" [(ngModel)]="closingDate" placeholder="Data de Encerramento" required>
+        </div>
+        <div>
+            <label for="paymentDate">Data de Pagamento</label>
+            <input class="input" type="number" id="paymentDate" name="paymentDate" [(ngModel)]="paymentDate" placeholder="Data de Pagamento" required>
+        </div>
+        
+
+        <button class="button" (onClick)="onSubmit()">Salvar</button>
     </div>
     `
 })
@@ -61,6 +77,16 @@ export class NewWalletComponent {
     protected closingDate = signal<{month: number, day: number}>({month: 1, day: 1})
     protected paymentDate = signal<{month: number, day: number}>({month: 1, day: 1})
     protected router = inject(Router)
+    
+    public icon = signal('')
+    public icons = [
+      'account_balance',
+      'savings',
+      'shopping_cart',
+      'payment',
+      'credit_card',
+      'attach_money'
+    ]
     
     // Valores auxiliares para o datepicker
     protected closingDateValue = signal<Date | null>(null)
